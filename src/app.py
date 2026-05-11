181b50bf4af838b6591d5212f3e6919cfeb39677
Jonathan <kakujonathan128@gmail.com>
2026-05-11 12:54:16 +0200

    feat(auth): adding CORS middleware

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 import config
 from shared.db import init_db
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.appointment_controller import appointments_router
 from controllers.auth_controller import auth_router
 from controllers.availability_controller import availabilities_router
@@ -28,6 +29,14 @@ def create_app() -> FastAPI:
         openapi_tags=meta["openapi_tags"],
         docs_url="/docs" if config.DOCS_ENABLED else None,
         redoc_url="/redoc" if config.DOCS_ENABLED else None,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:4200"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     register_error_handlers(app)
